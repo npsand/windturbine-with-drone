bf9b49ad00fa03a7553c8c031272b28e5e377fcc
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2024-06-14 14:06:09 +0300

    initial commit

---
+++ Drone/drone_input_params.m
@@ -0,0 +1,13 @@
+Ixx = 15.67*10^(-3);  % Quadrotor moment of inertia around X axis
+Iyy = 15.67*10^(-3);  % Quadrotor moment of inertia around Y axis
+Izz = 28.346*10^(-3);  % Quadrotor moment of inertia around Z axis
+Ax = 0.0; %Air resistance about X
+Ay = 0.0;%Air resistance about Y
+Az = 0.0;%Air resistance about Z
+Jr = 6.01*10^(-5);  % Total rotational moment of inertia around the propeller axis 6.5*10^(-6)
+b = 192.3208*10^(-7);  % Thrust factor
+d = 4.003*10^(-7);  % Drag factor
+l = 0.3;  % Distance to the center of the Quadrotor
+g = 9.81;   % Gravitational acceleration
+weight =8; %Newton  Weight=mg (from a scale)
+m = weight/g;  % Mass of the Quadrotor in Kg
---
+++ input_params.m
@@ -0,0 +1,3 @@
+run("WindTurbine\wind_turbine_inputs.m")
+run("Drone\drone_input_params.m")
+
